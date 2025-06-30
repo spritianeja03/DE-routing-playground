613783ad8a1b30c99328e310394910189e6217e8
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2025-06-30 15:34:28 +0530

    fix: main page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppLayout } from './components/AppLayout';
 import HomePage from './pages/HomePage';
 import { TooltipProvider } from '@/components/ui/tooltip';
+import Test from './pages/Test';
 
 const App: React.FC = () => {
   return (
@@ -10,6 +11,7 @@ const App: React.FC = () => {
       <Router>
         <Routes>
           <Route path="/" element={<AppLayout><HomePage /></AppLayout>} />
+          <Route path="*" element={<AppLayout><Test /></AppLayout>} />
         </Routes>
       </Router>
     </TooltipProvider>
