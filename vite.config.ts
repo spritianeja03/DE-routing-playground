0f739fe93095b72eb52ad7595a197712ffa934b9
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2025-07-02 18:06:05 +0530

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),cssInjectedByJsPlugin()],
   
   build:{
     lib: {
