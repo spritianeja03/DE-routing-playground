2d51ecece1b5ddb65c1512d311ad5db3a4dc9923
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2025-07-04 18:00:46 +0530

    fix: bundle

--- dist/index.es.js
+++ dist/index.es.js
@@ -32307,7 +32307,7 @@ const yie = Mf(
       variant: {
         default: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         // Standard flat default
-        primary: "de-bg-primary text-primary-foreground hover:bg-primary/90",
+        primary: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "border border-input-border bg-background hover:bg-accent hover:text-accent-foreground",
         secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
