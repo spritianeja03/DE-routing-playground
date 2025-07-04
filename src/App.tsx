11269c179dd1b4d0ed4830073bb58703758374e4
Riddhiagrawal001 <riddhi.agrawal2112@gmail.com>
2025-07-04 13:44:02 +0530

    fix: bundle

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { AppLayout } from './components/AppLayout';
 import HomePage from './pages/HomePage';
 import { TooltipProvider } from '@/components/ui/tooltip';
-import './style.css'
 
 interface AppProps {
   basename?: string;
